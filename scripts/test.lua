2645fb5769320c4e93046cde0af17a530d0ba8b7
FranchuFranchu <fff999abc999@gmail.com>
2021-02-25 16:14:00 -0300

    tweaked test.lua

--- scripts/test.lua
+++ scripts/test.lua
@@ -2,8 +2,8 @@ ember_waste_systems = {}
 
 for k, v in pairs(endless_sky.systems) do
 	if v.attributes:has("ember waste") then
-		ember_waste_systems[#ember_waste_systems+1] = k .. ", "
+		ember_waste_systems[#ember_waste_systems+1] = k
 	end
 end
 
-print(table.concat(ember_waste_systems))
+print(table.concat(ember_waste_systems, ", "))
